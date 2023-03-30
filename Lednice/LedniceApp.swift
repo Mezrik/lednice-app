a8ccbaa9d95993f9fc79fee2bebc52f10878e5a1
Martin Petr <martin.petr.276@gmail.com>
2023-03-30 23:53:56 +0200

    Initial Commit

---
+++ Lednice/LedniceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedniceApp.swift
+//  Lednice
+//
+//  Created by Martin Petr on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LedniceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
